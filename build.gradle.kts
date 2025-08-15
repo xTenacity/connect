9341abb09bed260c6b0a17fdf5e5db7ed79b02c5
xTenacity <83181683+xTenacity@users.noreply.github.com>
2025-08-14 21:54:43 -0700

    Create Spring Boot Project
    Easier to use lol

---
+++ build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.5.4"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.game"
+version = "0.0.1-SNAPSHOT"
+description = "Demo project for Spring Boot"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(24)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ src/main/java/com/game/spring_boot/Application.java
@@ -0,0 +1,14 @@
+package com.game.spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+		System.out.println("Game application started successfully!");
+	}
+
+}
