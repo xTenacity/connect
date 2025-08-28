5dfca6ed6c360988219d110f85d5b5159d06e253
xTenacity <83181683+xTenacity@users.noreply.github.com>
2025-08-27 20:04:57 -0700

    I have no idea what i did wrong but now it wont build

--- src/main/java/com/game/fourfront/controller/GameController.java
+++ src/main/java/com/game/fourfront/controller/GameController.java
@@ -1,10 +1,13 @@
 package com.game.fourfront.controller;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.game.fourfront.model.BoardState;
+import com.game.fourfront.model.Move;
+import com.game.fourfront.model.MoveResult;
 
 @RestController
 @RequestMapping("/api/game")
@@ -13,10 +16,12 @@ public class GameController {
     @GetMapping("/state")
     public BoardState getGameState() {
         // return current board
+        return new BoardState();
     }
 
     @PostMapping("/move")
     public MoveResult makeMove(@RequestBody Move move) {
         // process move
+        return new MoveResult();
     }
 }
